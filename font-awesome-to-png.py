ad28613322bf2852bc2c43aea424185ae055eca6
Michal Wojciechowski <odyniec@odyniec.net>
2014-05-25 18:04:38 +0200

    Creating icons using an alpha mask
    The previous way of generating icons produced ugly edges for larger
    icon sizes and bright colors (GitHub issue #37).

--- font-awesome-to-png.py
+++ font-awesome-to-png.py
@@ -476,19 +476,30 @@ def export_icon(icon, size, filename, font, color):
     # Get bounding box
     bbox = image.getbbox()
 
+    # Create an alpha mask
+    imagemask = Image.new("L", (size, size), 0)
+    drawmask = ImageDraw.Draw(imagemask)
+
+    # Draw the icon on the mask
+    drawmask.text(((size - width) / 2, (size - height) / 2), icons[icon],
+        font=font, fill=255)
+
+    # Create a solid color image and apply the mask
+    iconimage = Image.new("RGBA", (size,size), color)
+    iconimage.putalpha(imagemask)
+
     if bbox:
-        image = image.crop(bbox)
+        iconimage = iconimage.crop(bbox)
 
     borderw = int((size - (bbox[2] - bbox[0])) / 2)
     borderh = int((size - (bbox[3] - bbox[1])) / 2)
 
-    # Create background image
-    bg = Image.new("RGBA", (size, size), (0,0,0,0))
-
-    bg.paste(image, (borderw,borderh))
+    # Create output image
+    outimage = Image.new("RGBA", (size, size), (0,0,0,0))
+    outimage.paste(iconimage, (borderw,borderh))
 
     # Save file
-    bg.save(filename)
+    outimage.save(filename)
 
 
 class LoadCSSAction(argparse.Action):
